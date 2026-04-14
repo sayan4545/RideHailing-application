1427c9bc9c6653dfbf40cfd672df59fdab9e2cae
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-14 16:47:04 +0530

    Added dotenv dependency. in main file, configured the DotEnv file.

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/RidehailingMonolithApplication.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/RidehailingMonolithApplication.java
@@ -1,5 +1,6 @@
 package com.dev.sayan.ridehailing.ridehailingmonolith;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RidehailingMonolithApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries()
+                        .forEach(
+                                entry->System.setProperty(entry.getKey(),entry.getValue())
+                        );
+
         SpringApplication.run(RidehailingMonolithApplication.class, args);
     }
 
