0d1289b431938e8b6a19c4c709fb9f0575436566
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-14 14:04:12 +0530

    Basic set up the ride hailing platform application backend. set up the dependencies. created the packages

---
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/RidehailingMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sayan.ridehailing.ridehailingmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidehailingMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RidehailingMonolithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/sayan/ridehailing/ridehailingmonolith/RidehailingMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sayan.ridehailing.ridehailingmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidehailingMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
