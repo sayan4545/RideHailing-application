3b3c02dcc6811471df5b5303b083727f831ee0ba
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-17 02:08:02 +0530

    edited ModelMapper configuration to conver Point to PointDto and vice versa

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/configs/MapperConfig.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/configs/MapperConfig.java
@@ -1,5 +1,8 @@
 package com.dev.sayan.ridehailing.ridehailingmonolith.configs;
 
+import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.PointDto;
+import com.dev.sayan.ridehailing.ridehailingmonolith.utils.GeometryUtils;
+import org.locationtech.jts.geom.Point;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +12,22 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper mapper = new  ModelMapper();
+        mapper.typeMap(PointDto.class, Point.class).setConverter(context->{
+            PointDto pointDto = context.getSource();
+            return GeometryUtils.createPoint(pointDto);
+        });
+
+        mapper.typeMap(Point.class,PointDto.class)
+                .setConverter(context->{
+                    Point point = context.getSource();
+                    Double[] coordinates = {
+                            point.getX(),
+                            point.getY()
+                    };
+                    return new PointDto(coordinates);
+                });
+        return mapper;
     }
 
 
