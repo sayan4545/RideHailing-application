d2d76a8aa2d657f96d6e4285e84d22307638efe7
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-17 16:44:54 +0530

    The method consumes a RideRequest object not a dto

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/strategies/DriverMatchingStrategy.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/strategies/DriverMatchingStrategy.java
@@ -2,9 +2,10 @@ package com.dev.sayan.ridehailing.ridehailingmonolith.strategies;
 
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.DriverDto;
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.RideRequestDto;
+import com.dev.sayan.ridehailing.ridehailingmonolith.entities.RideRequest;
 
 import java.util.List;
 
 public interface DriverMatchingStrategy {
-    List<DriverDto> findMatchingDrivers(RideRequestDto rideRequestDto);
+    List<DriverDto> findMatchingDrivers(RideRequest rideRequest);
 }
