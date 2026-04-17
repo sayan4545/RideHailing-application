a61aa2c688f84c599fe88a28d66b24bd1ca918f9
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-17 16:48:38 +0530

    Changes in accordance of the stratey interface

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/strategies/implementations/DriverMatchingHighestRatedDriver.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/strategies/implementations/DriverMatchingHighestRatedDriver.java
@@ -2,6 +2,7 @@ package com.dev.sayan.ridehailing.ridehailingmonolith.strategies.implementations
 
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.DriverDto;
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.RideRequestDto;
+import com.dev.sayan.ridehailing.ridehailingmonolith.entities.RideRequest;
 import com.dev.sayan.ridehailing.ridehailingmonolith.strategies.DriverMatchingStrategy;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,7 @@ import java.util.List;
 @Service
 public class DriverMatchingHighestRatedDriver implements DriverMatchingStrategy {
     @Override
-    public List<DriverDto> findMatchingDrivers(RideRequestDto rideRequestDto) {
+    public List<DriverDto> findMatchingDrivers(RideRequest rideRequest) {
         return List.of();
     }
 }
