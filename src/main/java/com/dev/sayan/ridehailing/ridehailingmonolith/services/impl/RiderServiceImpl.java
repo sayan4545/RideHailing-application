c054451575a54d090b95baa001bf6dbb841856b2
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-17 16:46:50 +0530

    Constructed the requestRide method initially. It consumes a rideRequestDto. cacultaes fare, matches driver and returns a fully constructed rideRequestDto object

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/services/impl/RiderServiceImpl.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/services/impl/RiderServiceImpl.java
@@ -4,16 +4,47 @@ import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.DriverDto;
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.RideDto;
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.RideRequestDto;
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.RiderDto;
+import com.dev.sayan.ridehailing.ridehailingmonolith.entities.RideRequest;
+import com.dev.sayan.ridehailing.ridehailingmonolith.entities.enums.RideRequestStatus;
+import com.dev.sayan.ridehailing.ridehailingmonolith.repositories.RideRequestRepository;
 import com.dev.sayan.ridehailing.ridehailingmonolith.services.RiderService;
+import com.dev.sayan.ridehailing.ridehailingmonolith.strategies.DriverMatchingStrategy;
+import com.dev.sayan.ridehailing.ridehailingmonolith.strategies.FareCalculationStrategy;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class RiderServiceImpl implements RiderService {
+    private final ModelMapper modelMapper;
+    private final FareCalculationStrategy fareCalculationStrategy;
+    private final DriverMatchingStrategy driverMatchingStrategy;
+    private final RideRequestRepository rideRequestRepository;
+
+    public RiderServiceImpl(ModelMapper modelMapper, FareCalculationStrategy fareCalculationStrategy, DriverMatchingStrategy driverMatchingStrategy, RideRequestRepository rideRequestRepository) {
+        this.modelMapper = modelMapper;
+        this.fareCalculationStrategy = fareCalculationStrategy;
+        this.driverMatchingStrategy = driverMatchingStrategy;
+        this.rideRequestRepository = rideRequestRepository;
+    }
+
     @Override
     public RideRequestDto requestRide(RideRequestDto rideRequestDto) {
-        return null;
+        RideRequest rideRequest = modelMapper
+                .map(rideRequestDto, RideRequest.class);
+        log.info("RideRequestDto gets converted to Riderequest {}",rideRequest.toString());
+        rideRequest.setRideRequestStatus(RideRequestStatus.PENDING);
+        Double fare = fareCalculationStrategy.calculateFare(rideRequestDto);
+        rideRequest.setFare(fare);
+        // save this ride request
+        RideRequest persistedRideRequest = rideRequestRepository.save(rideRequest);
+
+        driverMatchingStrategy.findMatchingDrivers(rideRequest);
+
+        return modelMapper.map(persistedRideRequest, RideRequestDto.class);
     }
 
     @Override
