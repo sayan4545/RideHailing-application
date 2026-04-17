2ee7832c20c4b08bea3ae2c1fad6dec6f4396ce9
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-18 00:45:01 +0530

    Wrote a query to find top 10 nearest driver related to the pick up location and 10 km range

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/repositories/DriverRepository.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/repositories/DriverRepository.java
@@ -1,9 +1,25 @@
 package com.dev.sayan.ridehailing.ridehailingmonolith.repositories;
 
+import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.DriverDto;
 import com.dev.sayan.ridehailing.ridehailingmonolith.entities.Driver;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DriverRepository extends JpaRepository<Driver,Long> {
+    @Query(value = """
+        SELECT d.*, 
+        ST_Distance(d.current_location, :pickupLocation) AS distance
+        FROM drivers d
+        WHERE d.is_available = true
+        AND ST_DWithin(d.current_location, :pickupLocation, 1000)
+        ORDER BY distance
+        LIMIT 10
+    """, nativeQuery = true)
+
+    List<DriverDto> findTenNearestdrivers(Point pickUpLocation);
 }
