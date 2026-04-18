1993e5aac408a0a51904c4f7b57ef4207ce8fa54
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-19 02:07:23 +0530

    Added rider creation through user sign up. Here it is considered that any user that signs up, is a rider first

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/services/impl/AuthServiceImpl.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/services/impl/AuthServiceImpl.java
@@ -3,10 +3,13 @@ package com.dev.sayan.ridehailing.ridehailingmonolith.services.impl;
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.DriverDto;
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.SignUpDto;
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.UserDto;
+import com.dev.sayan.ridehailing.ridehailingmonolith.entities.Rider;
 import com.dev.sayan.ridehailing.ridehailingmonolith.entities.User;
 import com.dev.sayan.ridehailing.ridehailingmonolith.entities.enums.Roles;
+import com.dev.sayan.ridehailing.ridehailingmonolith.exceptions.RuntimeConflictException;
 import com.dev.sayan.ridehailing.ridehailingmonolith.repositories.UserRepository;
 import com.dev.sayan.ridehailing.ridehailingmonolith.services.AuthService;
+import com.dev.sayan.ridehailing.ridehailingmonolith.services.RiderService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +19,12 @@ import java.util.Set;
 public class AuthServiceImpl implements AuthService {
     private final ModelMapper modelMapper;
     private final UserRepository userRepository;
+    private final RiderService riderService;
 
-    public AuthServiceImpl(ModelMapper modelMapper, UserRepository userRepository) {
+    public AuthServiceImpl(ModelMapper modelMapper, UserRepository userRepository, RiderService riderService) {
         this.modelMapper = modelMapper;
         this.userRepository = userRepository;
+        this.riderService = riderService;
     }
 
     @Override
@@ -29,14 +34,18 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public UserDto signUp(SignUpDto signUpDto) {
-        User ifExistingUser = userRepository.findByEmail(signUpDto.getEmail());
-        if(ifExistingUser!=null){
-            throw new RuntimeException("User already exists by the email "+ signUpDto.getEmail());
-        }
+        User existingUser = userRepository
+                .findByEmail(signUpDto.getEmail())
+                .orElseThrow(()-> new RuntimeConflictException("User already present"));
+
         User user = modelMapper.map(signUpDto,User.class);
         user.setRoles(Set.of(Roles.RIDER));
         User savedUser = userRepository.save(user);
 
+        // create rider from user
+        Rider newRider = riderService.createNewRider(savedUser);
+
+        // TODO : CREATE WALLET RELATED SERVICES AND IMPLEMENTATION
         return modelMapper.map(savedUser, UserDto.class);
 
     }
