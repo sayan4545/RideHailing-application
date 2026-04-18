edcb6299e3663ddbc58940f497d53eda98ad5813
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-19 02:12:28 +0530

    Added @UtilityClass annotation

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/utils/GeometryUtils.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/utils/GeometryUtils.java
@@ -1,15 +1,17 @@
 package com.dev.sayan.ridehailing.ridehailingmonolith.utils;
 
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.PointDto;
+import lombok.experimental.UtilityClass;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.PrecisionModel;
 
+@UtilityClass
 public class GeometryUtils {
 
     public static Point createPoint(PointDto pointDto){
-        GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(),4328);
+        GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(),4326);
         Coordinate coordinate = new Coordinate(pointDto.getCoordinates()[0],
                 pointDto.getCoordinates()[1]);
         return geometryFactory.createPoint(coordinate);
