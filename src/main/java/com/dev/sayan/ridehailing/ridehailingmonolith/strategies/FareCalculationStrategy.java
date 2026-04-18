18cfb362e4f52def3675aae6fbb3c5a113360356
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-18 15:33:20 +0530

    Added a Ride fare multiplier and CalculateFare method to be implemented by different farecalculation services

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/strategies/FareCalculationStrategy.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/strategies/FareCalculationStrategy.java
@@ -1,7 +1,9 @@
 package com.dev.sayan.ridehailing.ridehailingmonolith.strategies;
 
 import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.RideRequestDto;
+import com.dev.sayan.ridehailing.ridehailingmonolith.entities.RideRequest;
 
 public interface FareCalculationStrategy {
-    Double calculateFare(RideRequestDto rideRequestDto);
+    Double RIDE_FARE_MULTIPLIER = 10.0;
+    Double calculateFare(RideRequest rideRequest);
 }
