b7947cb0875fe71e038b5e8fe3c2f0c50e76bf7b
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-19 02:31:11 +0530

    Created a api to test the signup process

---
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/controllers/AuthController.java
@@ -0,0 +1,26 @@
+package com.dev.sayan.ridehailing.ridehailingmonolith.controllers;
+
+import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.SignUpDto;
+import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.UserDto;
+import com.dev.sayan.ridehailing.ridehailingmonolith.services.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+    @PostMapping("/signUp")
+    public ResponseEntity<UserDto> signUp(@RequestBody SignUpDto signUpDto){
+        return new ResponseEntity<>(authService.signUp(signUpDto), HttpStatus.CONFLICT);
+    }
+}
