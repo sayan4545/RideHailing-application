ed60027e045e34297067ec60da824ba16ac7a792
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-19 18:35:17 +0530

    Called the ORSM api using Restclient to calculate the distance between two coordinates , the source and pick up location

--- src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/services/impl/DistanceServiceORSMImpl.java
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/services/impl/DistanceServiceORSMImpl.java
@@ -1,14 +1,31 @@
 package com.dev.sayan.ridehailing.ridehailingmonolith.services.impl;
 
+import com.dev.sayan.ridehailing.ridehailingmonolith.dtos.ORSMResponseDto;
 import com.dev.sayan.ridehailing.ridehailingmonolith.services.DistanceService;
 import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class DistanceServiceORSMImpl implements DistanceService {
+    private static final String ORSM_API = "http://router.project-osrm.org/route/v1/driving/";
     @Override
     public Double calculateDistance(Point src, Point dest) {
         // call the third party api
-        return 0.0;
+        try{
+            String uri = src.getX()+","+src.getY()+";"+dest.getX()+","+dest.getY();
+            ORSMResponseDto responseDto =
+                    RestClient.builder()
+                            .baseUrl(ORSM_API)
+                            .build()
+                            .get()
+                            .uri(uri)
+                            .retrieve()
+                            .body(ORSMResponseDto.class);
+            return responseDto.getRoutes().get(0).getDistance()/1000.0;
+        } catch (Exception e) {
+            throw new RuntimeException("No routes availbale"+ e.getMessage());
+        }
+
     }
 }
