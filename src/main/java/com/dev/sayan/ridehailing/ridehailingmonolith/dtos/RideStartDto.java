920f8847922d5e216d4eda8b14915c2b254fcec9
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-21 19:39:28 +0530

    Added a dto to transfer the stratRide object between all the services

---
+++ src/main/java/com/dev/sayan/ridehailing/ridehailingmonolith/dtos/RideStartDto.java
@@ -0,0 +1,12 @@
+package com.dev.sayan.ridehailing.ridehailingmonolith.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RideStartDto {
+    private String otp;
+}
